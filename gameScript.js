e1644e16079bea6faabaeac63bb8df866ec35cf3
harshit-katyal <hkatyal345@gmail.com>
2021-10-01 15:24:42 +0530

    Update gameScript.js

--- gameScript.js
+++ gameScript.js
@@ -25,7 +25,7 @@ window.onload = function() {
     $("#ol").html(`<center><div id="inst"><h3>Welcome !</h3>Instructions For Game<br/><br/><li>Make pairs of similiar blocks by flipping them.</li><li>To flip a block you can click on it.</li><li>If two blocks you clicked are not similar, they will be flipped back.</li><p style="font-size:18px;">Click one of the following mode to start the game.</p></div><button onclick="start(3, 4)">3 x 4</button> <button onclick="start(4, 4)" style="w">4 x 4</button><button onclick="start(4, 5)">4 x 5</button><button onclick="start(5, 6)">5 x 6</button><button onclick="start(6, 6)">6 x 6</button></center>`);
 }
 
-//Starting the game
+//Starting
 function start(r,l) {
     //Timer and moves
     min=0, sec=0, moves=0;
